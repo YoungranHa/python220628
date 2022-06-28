309cda1bea97beea304705d05e6341865f9e0fb9
hyr0419 <hyr0419@naver.com>
2022-06-28 11:54:38 +0900

    함수연습 1 2

---
+++ function1.py
@@ -0,0 +1,17 @@
+# function1.py
+# 함수를 정의
+def times(a,b):
+    return a*b
+
+# 함수를 호출
+print(times(3,4))
+
+
+#함수 정ㅇ의
+def setValue(x):
+    print("값",x)
+
+#함수 호출
+result=setValue(5)
+print(result)
+
