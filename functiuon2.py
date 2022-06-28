5a0be86a443d71b6018444100b0a016476a3e396
hyr0419 <hyr0419@naver.com>
2022-06-28 11:55:35 +0900

    함수2

---
+++ functiuon2.py
@@ -0,0 +1,17 @@
+# functiuon2.py
+
+def swap(x,y):
+    return y,x
+
+
+result=swap(5,6)
+print(result)
+
+def intersect(prelist, postlist):
+    result=[]
+    for x in prelist:
+        if x in postlist and x not in result:
+            result.append(x)
+        return result
+
+print(intersect("HAM","SPAM"))
